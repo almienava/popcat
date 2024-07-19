e77624795998f0094c94fa2d756fadea533f0f2b
Haldad Almi <61896664+almienava@users.noreply.github.com>
2024-07-19 20:06:39 +0700

    Update mains.py
    add code for thread process management

--- mains.py
+++ mains.py
@@ -2,20 +2,37 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 import sys
+from threading import Thread
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--mute-audio')
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+def init_chromeDriver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument('--mute-audio')
+    chrome_options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    return driver
 
-app = '//*[@id="app"]'
-url_= "https://popcat.click/"
-driver.get(url_)
-cat = driver.find_element('xpath',app)
-sys.stdout.reconfigure(line_buffering=True)
-count = 0
-print("Bot PopCat Di Mulai")
-while True:
-    cat.click()
-    count+=1
-    print(f"click yang ke {count}")
+def click_process(num_thread):
+    driver = init_chromeDriver()
+    app = '//*[@id="app"]'
+    url_= "https://popcat.click/"
+    driver.get(url_)
+    cat = driver.find_element('xpath',app)
+    sys.stdout.reconfigure(line_buffering=True)
+    count = 0
+    print("Bot PopCat Di Mulai")
+    while True:
+        cat.click()
+        count+=1
+        print(f"[thread[{num_thread}]]total clicked {count}")
+
+if __name__ == "__main__":
+    total_thread = int(input('input total thread : ')
+    for x in range(total_thread):
+        t = Thread(target=click_process,args=(x,))
+        t.start()
